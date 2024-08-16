3d936c29ae98807f2c528f7b840010a6465c8bd2
“babich-maria” <Ljurka@gmail.com>
2024-08-16 23:58:56 +0200

    feat: add func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (number) {
+  const str = number.toString();
+
+  // Reverse the string
+  const reversedStr = str.split('').reverse().join('');
+
+  // Convert the reversed string back to a number
+  const reversedNumber = parseInt(reversedStr, 10);
+
+  // Return the reversed number
+  return reversedNumber;
 }
